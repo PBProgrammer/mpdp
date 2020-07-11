afd460dc2b1b28240cfb4c2d2b8aa930bcfc611d
PBProgrammer <paraupleines@gmail.com>
2020-07-11 20:58:51 +0200

    adding necessary files

---
+++ src/main/java/com/mpdp/mpdp/MpdpApplication.java
@@ -0,0 +1,13 @@
+package com.mpdp.mpdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpdp/mpdp/MpdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpdp.mpdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
